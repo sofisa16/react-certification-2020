4b20e64b77b2e342523e04179a780ab6338a0fdf
Sofía Isadora Padilla Muñoz <sofisa16@gmail.com>
2021-03-19 21:13:58 -0700

    Jest tests working.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Dark mode text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/Dark mode/i);
+  expect(text).toBeInTheDocument();
 });
